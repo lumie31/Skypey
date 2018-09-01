030391f715145a57608921ec05356ac4869fa755
Olumide Okedusi <odokedusi@sidmach.com>
2018-09-01 23:15:08 +0100

    Dispatch action + reducer logic

--- src/containers/User.js
+++ src/containers/User.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './User.css'
+import store from "../store";
+import { setActiveUserId } from "../actions";
 
 const User = ({ user }) => {
   const { name, profile_pic, status } = user
 
   return (
-    <div className='User'>
+    <div className='User' onClick={handleUserClick.bind(null, user)}>
       <img src={profile_pic} alt={name} className='User__pic' />
       <div className='User__details'>
         <p className='User__details-name'>{name}</p>
@@ -13,6 +15,10 @@ const User = ({ user }) => {
       </div>
     </div>
   )
+}
+
+function handleUserClick({ user_id }) {
+  return store.dispatch(setActiveUserId(user_id))
 }
 
 export default User;
