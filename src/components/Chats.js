6aba4f8a9728adb15005e58bc0a4aed841d18ed2
Olumide <olumideokedusi@gmail.com>
2019-01-06 07:21:18 +0100

    Add Styling(css) & interactivity(js) to Chats component

---
+++ src/components/Chats.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Chats.css'
+
+const Chats = ({ messages }) => {
+  const { text, is_user_msg } = messages
+
+  return (
+    <span className=""></span>
+  )
+}
+
+export default Chats
